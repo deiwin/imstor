d267c58bef7894aeb878642cf06819a756ea86b6
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-03-24 20:51:41 +0200

    initial idea

--- config.go
+++ config.go
@@ -7,11 +7,21 @@ var (
 )
 
 type Config struct {
-	RootPath string
+	RootPath  string
+	CopySizes []Size
 }
 
-func NewConfig() *Config {
+func NewConfig(copySizes []Size) *Config {
 	return &Config{
-		RootPath: rootPathEnvProperty.Value(),
+		RootPath:  rootPathEnvProperty.Value(),
+		CopySizes: copySizes,
 	}
+}
+
+// Size specifies a set of dimensions and a name that a copy of an image will
+// be stored as
+type Size struct {
+	Name   string
+	Height uint
+	Width  uint
 }
